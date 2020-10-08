8177e5c56625247893b3361587e05ead9fc5dd45
anubhav singh <anubhavsingh230@gmail.com>
2020-10-08 07:26:44 +0530

    practice: for this keyword

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 //ES6 - errow function
+
+//******** arrow function *********//
 const jobs = [
     { id: 1, isActive: true },
     { id: 1, isActive: true },
@@ -13,3 +15,27 @@ const activeJobs2 = jobs.filter(job => job.isActive);
 
 console.log(activeJobs);
 console.log(activeJobs2);
+
+//******** this keyword *********//
+
+const person = {
+    name: 'anubhav',
+    walk() {
+        console.log(this) //this keyword always returs to the current object but not in javascipt
+    }
+};
+
+//calling walk with .
+
+person.walk();
+
+// defining a new constant walk and set it to person.walk
+
+const walk = person.walk;
+
+walk(); // it will not return expecte result because strict mode
+
+// but if we use .bind to define to 
+const walk2 = person.walk.bind(person)
+
+walk2(); // now it will same result as person.walk();
